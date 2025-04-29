735cc6b99a92624073115c2035f4d69d93840baf
罗远祥 <1141306760@qq.com>
2025-04-29 14:46:24 +0800

    refactor(components): 优化 LAStatis 组件中的统计代码渲染- 将分散的 Script 组件合并为一个，减少组件数量- 使用 dangerouslySetInnerHTML 统一设置内联脚本，提高代码可读性 - 将统计代码封装为单独的变量，便于管理和修改

--- src/components/LAStatis/index.tsx
+++ src/components/LAStatis/index.tsx
@@ -1,20 +1,18 @@
 import Script from "next/script"
 
 export default () => {
+    const statsCode = `
+    <!-- 51.la 网站统计代码 -->
+    <script type="text/javascript" src="https://sdk.51.la/js-sdk-pro.min.js"></script>
+    <script type="text/javascript">
+      LA.init({id:"3FYZE9zfaINipTml",ck:"3FYZE9zfaINipTml"})
+    </script>
+  `
     return (
-        <>
-            <Script
-                id="51la-sdk"
-                strategy="afterInteractive"
-                src="//sdk.51.la/js-sdk-pro.min.js"
-            />
-            <Script
-                id="51la-init"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `LA.init({id:"3FYZE9zfaINipTml", ck:"3FYZE9zfaINipTml"})`,
-                }}
-            />
-        </>
+        <Script
+            id="stats-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{__html: statsCode}}
+        />
     )
 }
