1e04853d34355ef71dca80f5393fd2caba7913b6
罗远祥 <1141306760@qq.com>
2025-04-29 14:53:25 +0800

    refactor: 移除 51.la 网站统计组件
    - 删除了 LAStatis 组件的实现文件
    -从布局中移除了 LAStatis 组件的引用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,6 @@ import "@/styles/index.scss";
 import "@/styles/tailwind.scss";
 import BaiduStatis from '@/components/BaiduStatis';
 import { Analytics } from "@vercel/analytics/react"
-import LAStatis from "@/components/LAStatis";
 
 // 加载本地字体
 const LXGWWenKai = localFont({
@@ -53,8 +52,6 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
 
         {/* 百度统计 */}
         <BaiduStatis />
-        {/* 51.la统计 */}
-        <LAStatis/>
         <Analytics/>
       </head>
 
