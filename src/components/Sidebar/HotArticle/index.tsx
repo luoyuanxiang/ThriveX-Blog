00d604c98b8482f547ec92c7299b5e5f081aff30
罗远祥 <1141306760@qq.com>
2025-09-11 17:33:21 +0800

    refactor(Sidebar/HotArticle):优化RandomArticle组件的代码结构和逻辑
    - 将导入语句格式化为单行
    - 明确指定RandomArticle函数返回Promise<JSX.Element>
    - 优化ids数组生成逻辑，使用map方法直接转换
    - 使用forEach方法替代filter方法来构建推荐文章列表
    - 调整Image组件的属性书写格式，移除不必要的空格- 保持原有功能不变，仅对代码风格进行调整以提高可读性和一致性

--- src/components/Sidebar/HotArticle/index.tsx
+++ src/components/Sidebar/HotArticle/index.tsx
@@ -1,42 +1,49 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { getWebConfigDataAPI } from '@/api/config';
-import { getArticleListAPI } from '@/api/article';
-import { IoIosArrowForward } from 'react-icons/io';
+import {getWebConfigDataAPI} from '@/api/config';
+import {getArticleListAPI} from '@/api/article';
+import {IoIosArrowForward} from 'react-icons/io';
 import FireSvg from '@/assets/svg/other/fire.svg';
-import { Theme } from '@/types/app/config';
-import { Article } from '@/types/app/article';
+import {Theme} from '@/types/app/config';
+import {Article} from '@/types/app/article';
+import {JSX} from 'react';
 
-const RandomArticle = async () => {
-  const {
-    data: { value: theme },
-  } = (await getWebConfigDataAPI<{ value: Theme }>('theme')) || { data: { value: {} as Theme } };
-  const { data: article } = (await getArticleListAPI()) || { data: [] as Article[] };
-  const ids = theme.reco_article.map((item) => Number(item)) || [];
-  const list = article?.filter((item: Article) => ids.includes(item.id as number)) || [];
+const RandomArticle: () => Promise<JSX.Element> = async (): Promise<JSX.Element> => {
+    const {
+        data: {value: theme},
+    } = (await getWebConfigDataAPI<{ value: Theme }>('theme')) || {data: {value: {} as Theme}};
+    const {data: article} = (await getArticleListAPI()) || {data: [] as Article[]};
+    const ids: number[] = theme.reco_article.map((item: number): number => Number(item)) || [];
+    const recommendArticleData: Article[] = []
+    ids.forEach((id: number): void => {
+        const a: Article | undefined = article?.find((item: Article): boolean => item.id === id)
+        if (a) {
+            recommendArticleData.push(a)
+        }
+    })
+    return (
+        <div className="hotArticleComponent">
+            <div className="flex flex-col tw_container bg-white dark:bg-black-b p-4 mb-5 tw_title">
+                <div className="tw_title w-full dark:text-white">
+                    <Image src={FireSvg} alt="作者推荐" width={30} height={20}/>
+                    <span> 作者推荐</span>
+                </div>
 
-  return (
-    <div className="hotArticleComponent">
-      <div className="flex flex-col tw_container bg-white dark:bg-black-b p-4 mb-5 tw_title">
-        <div className="tw_title w-full dark:text-white">
-          <Image src={FireSvg} alt="作者推荐" width={30} height={20} />
-          <span> 作者推荐</span>
-        </div>
-
-        {/* 文章列表 */}
-        <div className="w-full">
-          {list?.map((item: Article) => (
-            <div key={item.id}>
-              <Link href={`/article/${item.id}`} target="_blank" className="w-full flex items-center py-2 text-gray-600 dark:text-[#8c9ab1] text-sm transition-[padding] hover:!text-primary hover:pl-2">
-                <IoIosArrowForward className="text-lg mr-1" />
-                <span className="w-full line-clamp-1">{item.title}</span>
-              </Link>
+                {/* 文章列表 */}
+                <div className="w-full">
+                    {recommendArticleData?.map((item: Article) => (
+                        <div key={item.id}>
+                            <Link href={`/article/${item.id}`} target="_blank"
+                                  className="w-full flex items-center py-2 text-gray-600 dark:text-[#8c9ab1] text-sm transition-[padding] hover:!text-primary hover:pl-2">
+                                <IoIosArrowForward className="text-lg mr-1"/>
+                                <span className="w-full line-clamp-1">{item.title}</span>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default RandomArticle;
